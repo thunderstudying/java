50ebee754a1a28b0ef5a08ea4034cc70da7a832a
thunder <39192865+thunderstudying@users.noreply.github.com>
2018-10-01 17:04:34 +0800

    practice3

---
+++ App/Point Song/Song.java
@@ -0,0 +1,25 @@
+package com.wq;
+/**
+ * 
+ * @author wq
+ * @date October 1,2018
+ */
+public class Song {
+	String songName;//歌曲名	
+	String singer;//歌手
+	double time;//歌曲时长
+	public Song(String songName,String singer,double time)//构造函数
+	{
+		this.songName=songName;
+		this.singer=singer;
+		this.time=time;
+	}
+	public String toString()//重写Song类对象的toString()函数，以便在listSong中打印歌曲信息
+	{
+		StringBuffer s = new StringBuffer(""); 
+		s.append("歌曲名"+ this.songName); 
+		s.append("歌手"+ this.singer); 
+		s.append("歌曲时长"+ this.time); 
+		return s.toString();//返回重写后的字符串
+	}	
+}
