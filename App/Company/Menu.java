3d0275d41f7e443731c1232da8415e3c5105b195
thunder <39192865+thunderstudying@users.noreply.github.com>
2018-10-14 12:54:13 +0800

    Menu

---
+++ App/Company/Menu.java
@@ -0,0 +1,18 @@
+package com.wq;
+/**
+ * @author wq
+ * @date October 14,2018
+ */
+public class Menu {
+	static void ShowMainMenu() {
+		System.out.println("\n");
+		System.out.println("*******************公司******************");
+		System.out.println("* 1  公司全年发放的薪水	*");
+		System.out.println("* 2  公司的年终利润		*");
+		System.out.println("* 3  公司的股票分红		*");
+		System.out.println("* 4  根据姓名查询工资等信息	*");
+		System.out.println("* 5  查询所有职员某月工资	*");
+		System.out.println("* 6  退出			*");
+		System.out.println("***************************************************");
+	}
+}
